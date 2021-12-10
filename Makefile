e6bce2a02084ec56e6ec23ab967e97a0c0d19efc
Georgii Korshunov <kipelovets@mail.ru>
2021-12-10 22:01:39 +0100

    Day1.1

---
+++ Makefile
@@ -0,0 +1,3 @@
+day1:
+	yarn tsc
+	node build/day1.js
---
+++ src/day1.ts
@@ -0,0 +1,13 @@
+import * as fs from "fs";
+
+const lines = fs.readFileSync("data/day1.txt", "utf8").split("\n");
+
+const result = lines.reduce((prev, curr, ind) => {
+  if (ind > 0 && Number.parseInt(curr) > Number.parseInt(lines[ind - 1])) {
+    return prev + 1;
+  }
+
+  return prev;
+}, 0);
+
+console.log(`Answer: ${result}`);
