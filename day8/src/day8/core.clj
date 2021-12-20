8d49de3adcd2b4ed31007eeff209d20b08aa260d
Georgii Korshunov <gkorshunov@gog.com>
2021-12-21 00:51:20 +0100

    Day 8.2

--- day8/src/day8/core.clj
+++ day8/src/day8/core.clj
@@ -1,5 +1,17 @@
 (ns day8.core
-  (:require [clojure.string :as s]))
+  (:require [clojure.string :as s]
+            [clojure.set :as set]))
+
+(def digit-segments {0 "abcefg"
+                     1 "cf"
+                     2 "acdeg"
+                     3 "acdfg"
+                     4 "bcdf"
+                     5 "abdfg"
+                     6 "abdefg"
+                     7 "acf"
+                     8 "abcdefg"
+                     9 "abcdfg"})
 
 (defn parse-line [line]
   (let [vals (s/split line #" ")]
@@ -19,3 +31,51 @@
 (defn unique-count [input]
   (let [counts (map unique-count-line input)]
     (reduce + counts)))
+
+(defn single-segment-stats [char line]
+  (let [digits (doall (s/join "" (take 10 line)))]
+    (count (re-seq (re-pattern char) digits))))
+
+(def all-segments ["a" "b" "c" "d" "e" "f" "g"])
+
+(defn segment-stats [line]
+  (map #(single-segment-stats % line) all-segments))
+
+(defn index-of [e coll]
+  (first (keep-indexed #(if (= e %2) %1) coll)))
+
+(defn find-of-length [len col]
+  (first (filter #(= len (count %)) col)))
+
+(defn clean-vals [s col]
+  (s/replace s (re-pattern (str "[" (s/join "" (vals col)) "]")) ""))
+
+(defn detect-mixed-segments [line]
+  (let [signal-pattern (take 10 line)
+        stats (segment-stats line)
+        result {"b" (get all-segments (index-of 6 stats))
+                "e" (get all-segments (index-of 4 stats))
+                "f" (get all-segments (index-of 9 stats))}
+        pattern-for-one (find-of-length 2 signal-pattern)
+        pattern-for-four (find-of-length 4 signal-pattern)
+        result (assoc result
+                      "c" (clean-vals pattern-for-one result))
+        result (assoc result
+                      "d" (clean-vals pattern-for-four result))
+        result (assoc result
+                      "a" (clean-vals (find-of-length 3 signal-pattern) result))
+        result (assoc result
+                      "g" (clean-vals (find-of-length 7 signal-pattern) result))]
+    result))
+
+(defn replace-with-map [s col]
+  (let [inv-map (set/map-invert col)]
+    (s/join "" (sort (map #(get inv-map (str %)) s)))))
+
+(defn decode-output-digits [line]
+  (let [segments-map (detect-mixed-segments line)
+        output-digits (take-last 4 line)
+        unmixed-segments (map #(replace-with-map % segments-map) output-digits)
+        inv-segments (set/map-invert digit-segments)
+        digits (map #(get inv-segments %) unmixed-segments)]
+    (Integer/parseInt (s/join "" (map str digits)))))
--- day8/test/day8/core_test.clj
+++ day8/test/day8/core_test.clj
@@ -1,9 +1,23 @@
 (ns day8.core-test
   (:require [clojure.test :refer :all]
-            [day8.core :refer :all]))
+            [day8.core :refer :all]
+            [clojure.string :as s]))
 
-(def sample-input "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")
-(def sample-input-2 "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+(def proper-stats {"a" 8
+                   "b" 6 ;; 
+                   "c" 8
+                   "d" 7
+                   "e" 4 ;; 
+                   "f" 9 ;; 
+                   "g" 7})
+(def proper-character-stats (vals proper-stats))
+
+(def proper-digit-segments (vals digit-segments))
+
+(def sample-input
+  "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")
+(def sample-input-2
+  "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
 edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
 fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
 fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
@@ -78,6 +92,75 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
     (is (= 26 (unique-count (parse-input sample-input-2))))
     (is (= 2 (unique-count (parse-input sample-input-3))))))
 
-(deftest test-part-1
+(deftest test-solution-part-1
   (testing "part-1"
     (is (= 409 (unique-count (parse-input (slurp "input")))))))
+
+(deftest test-character-stats
+  (testing "single charater stats"
+    (is (= 8 (single-segment-stats "a" proper-digit-segments))))
+
+  (testing "proper layout character stats"
+    (let [char-counts (segment-stats proper-digit-segments)]
+      (is (= proper-character-stats char-counts))))
+
+  (testing "sample single character stats"
+    (is (=
+         8
+         (single-segment-stats "a" (first (parse-input sample-input))))))
+
+  (testing "sample stats"
+    (let [stats (segment-stats (first (parse-input sample-input)))]
+      (is (=
+           [8 9 7 8 6 7 4]
+           stats))
+      (is (=
+           (sort proper-character-stats)
+           (sort stats))))))
+
+(deftest test-clean-vals
+  (testing "clean-vals"
+    (is (= "cdf" (clean-vals "abcdf" {"x" "a" "y" "b"})))))
+
+(def first-sample-line (first (parse-input sample-input)))
+
+(deftest test-detect-mixed-segments
+  (testing "detect-mixed-segments sample"
+    (is (=
+         {"b" "e", "e" "g", "f" "b", "c" "a", "d" "f", "a" "d", "g" "c"}
+         (detect-mixed-segments first-sample-line)))))
+
+(deftest test-replace-with-map
+  (testing "replace-with-map"
+    (is (=
+         "abc"
+         (replace-with-map "sxy" {"a" "x" "b" "y" "y" "w" "c" "s"})))))
+
+(deftest test-decode-output-digits
+  (testing "decode-output-digits sample"
+    (is (=
+         5353
+         (decode-output-digits first-sample-line))))
+
+  (testing "decode sample 2"
+    (let [decoded (map decode-output-digits (parse-input sample-input-2))]
+      (is (=
+           [8394
+            9781
+            1197
+            9361
+            4873
+            8418
+            4548
+            1625
+            8717
+            4315]
+           decoded))
+      (is (=
+           61229
+           (reduce + decoded)))))
+
+  (testing "solution part 2"
+    (is (=
+         1024649
+         (reduce + (map decode-output-digits (parse-input (slurp "input"))))))))
