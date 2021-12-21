d3076f4bbc99b52ad78cafc76a47d4ed03b680a7
Georgii Korshunov <kipelovets@mail.ru>
2021-12-21 19:06:24 +0100

    Day 10.2

--- day10/src/day10/core.clj
+++ day10/src/day10/core.clj
@@ -36,12 +36,29 @@
     (if (str/starts-with? stack corrupted-literal)
       (str (first (subs stack (count corrupted-literal)))))))
 
-(defn points [line]
+(defn corrupted-points [line]
   (let [chunk (corrupted-chunk line)
         scores {")" 3 "]" 57 "}" 1197 ">" 25137}]
     (if (= nil chunk)
       0
       (scores chunk))))
 
-(defn all-points [lines]
-  (reduce + (map points lines)))
+(defn all-corrupted-points [lines]
+  (reduce + (map corrupted-points lines)))
+
+(defn incomplete-points [line]
+  (let [stack (reduce stack-reducer line)
+        scores {")" 1 "]" 2 "}" 3 ">" 4}
+        matching-chunks (map #(pairs (str %)) stack)
+        input (cons 0 (reverse matching-chunks))]
+    (if (str/starts-with? stack corrupted-literal)
+      0
+      (reduce (fn [acc chunk]
+                (let [points (scores chunk)]
+                  (+ points (* 5 acc))))
+              input))))
+
+(defn all-incomplete-points [nav]
+  (let [points (filter #(> % 0) (sort (map incomplete-points nav)))
+        midpoint (int (Math/floor (/ (count points) 2)))]
+    (nth points midpoint)))
--- day10/test/day10/core_test.clj
+++ day10/test/day10/core_test.clj
@@ -49,5 +49,18 @@
          (map corrupted-chunk example-nav))))
 
   (testing "points"
-    (is (= 26397 (all-points example-nav)))
-    (is (= 271245 (all-points real-nav)))))
+    (is (= 26397 (all-corrupted-points example-nav)))
+    (is (= 271245 (all-corrupted-points real-nav))))
+
+  (testing "incomplete"
+    (is (=
+         [288957 5566 0 1480781 0 0 995444 0 0 294]
+         (map #(incomplete-points %) example-nav))))
+
+  (testing "all-incomplete-points"
+    (is (=
+         288957
+         (all-incomplete-points example-nav)))
+    (is (=
+         1685293086
+         (all-incomplete-points real-nav)))))
